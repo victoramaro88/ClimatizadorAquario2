20bd4b5c83cbae18c50f359a187e7a19f5f45e70
DESKTOP-G1GO29V\Victor Amaro <amaro.victor@hotmail.com>
2020-11-28 19:28:47 -0300

    Criação do ManterConfig

--- ClimatizadorAquario2/Controllers/ConfigController.cs
+++ ClimatizadorAquario2/Controllers/ConfigController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClimatizadorAquario2.Models;
 using ClimatizadorAquario2.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -33,7 +34,41 @@ namespace ClimatizadorAquario2.Controllers
             {
                 return BadRequest(ex.Message);
                 throw;
-            }            
+            }
+        }
+
+        [HttpGet("{idConfig}/{descLocal}/{dataAtualizacao}/{infoMACUltimoAcesso}/{temperatura}/{flagIluminacao}/{flagAquecedor}/{flagResfriador}/{flagFiltro}/{flagEncher}/{flagEsvaziar}")]
+        [Produces("application/json")]
+        public IActionResult ManterInfo(int idConfig, string descLocal, DateTime dataAtualizacao, string infoMACUltimoAcesso, decimal temperatura,
+            bool flagIluminacao, bool flagAquecedor, bool flagResfriador, bool flagFiltro, bool flagEncher, bool flagEsvaziar)
+        {
+            ConfigModel objModel = new ConfigModel();
+            try
+            {
+                objModel.idConfig = idConfig;
+                objModel.descLocal = descLocal;
+                objModel.dataAtualizacao = dataAtualizacao;
+                objModel.infoMACUltimoAcesso = infoMACUltimoAcesso;
+                objModel.temperatura = temperatura;
+                objModel.flagIluminacao = flagIluminacao;
+                objModel.flagAquecedor = flagAquecedor;
+                objModel.flagResfriador = flagResfriador;
+                objModel.flagFiltro = flagFiltro;
+                objModel.flagEncher = flagEncher;
+                objModel.flagEsvaziar = flagEsvaziar;
+
+                var ret = _configRepo.ManterInfo(objModel);
+
+                if(ret == "OK")
+                    return Ok(ret);
+                else
+                    return BadRequest(ret);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw;
+            }
         }
     }
 }
--- ClimatizadorAquario2/Repository/ConfigRepository.cs
+++ ClimatizadorAquario2/Repository/ConfigRepository.cs
@@ -67,5 +67,84 @@ namespace ClimatizadorAquario2.Repository
 
             return objRet;
         }
+
+        public string ManterInfo(ConfigModel objModel)
+        {
+            string objRet = "";
+            SqlDataReader reader = null;
+            var query = "";
+            try
+            {
+                using (SqlConnection con = new SqlConnection(_bdAquario))
+                {
+                    query = @"
+                            DECLARE @valorRetorno int;
+
+                            SET @valorRetorno = (SELECT COUNT([idConfig]) AS Retorno 
+						                            FROM [aquario].[amaro.victor].[ConfAquario]
+						                            WHERE [idConfig] = " + objModel.idConfig + @")
+
+                            IF @valorRetorno = 1
+	                            BEGIN TRY  
+		                            UPDATE [amaro.victor].[ConfAquario]
+		                               SET [descLocal] = '" + objModel.descLocal + @"'
+			                              ,[dataAtualizacao] = '" + objModel.dataAtualizacao + @"'
+			                              ,[infoMACUltimoAcesso] = '" + objModel.infoMACUltimoAcesso + @"'
+			                              ,[temperatura] = " + objModel.temperatura.ToString().Replace(",", ".") + @"
+			                              ,[flagIluminacao] = " + (objModel.flagIluminacao ? 1 : 0) + @"
+			                              ,[flagAquecedor] = " + (objModel.flagAquecedor ? 1 : 0) + @"
+			                              ,[flagResfriador] = " + (objModel.flagResfriador ? 1 : 0) + @"
+			                              ,[flagFiltro] = " + (objModel.flagFiltro ? 1 : 0) + @"
+			                              ,[flagEncher] = " + (objModel.flagEncher ? 1 : 0) + @"
+			                              ,[flagEsvaziar] = " + (objModel.flagEsvaziar ? 1 : 0) + @"
+		                             WHERE [idConfig] = " + objModel.idConfig + @"
+		                             SELECT 'OK' AS Retorno
+	                            END TRY  
+	                            BEGIN CATCH
+		                            SELECT  'ERRO SQL: ' + CONVERT(varchar(10), ERROR_NUMBER()) + ' -> ' + ERROR_MESSAGE() AS Retorno
+	                            END CATCH;
+                            ELSE
+	                            BEGIN TRY
+		                            INSERT INTO [amaro.victor].[ConfAquario]
+			                               ([descLocal],[dataAtualizacao],[infoMACUltimoAcesso],[temperatura]
+			                               ,[flagIluminacao],[flagAquecedor],[flagResfriador],[flagFiltro]
+			                               ,[flagEncher],[flagEsvaziar])
+		                             VALUES
+			                               ('" + objModel.descLocal + @"'
+			                               ,'" + objModel.dataAtualizacao + @"'
+			                               ,'" + objModel.infoMACUltimoAcesso + @"'
+			                               ," + objModel.temperatura.ToString().Replace(",", ".") + @"
+			                               ," + (objModel.flagIluminacao ? 1 : 0) + @"
+			                               ," + (objModel.flagAquecedor ? 1 : 0) + @"
+			                               ," + (objModel.flagResfriador ? 1 : 0) + @"
+			                               ," + (objModel.flagFiltro ? 1 : 0) + @"
+			                               ," + (objModel.flagEncher ? 1 : 0) + @"
+			                               ," + (objModel.flagEsvaziar ? 1 : 0) + @")
+		                             SELECT 'OK' AS Retorno
+	                            END TRY  
+	                            BEGIN CATCH
+		                            SELECT  'ERRO SQL: ' + CONVERT(varchar(10), ERROR_NUMBER()) + ' -> ' + ERROR_MESSAGE() AS Retorno
+	                            END CATCH;";
+
+                    SqlCommand com = new SqlCommand(query, con);
+                    con.Open();
+
+                    reader = com.ExecuteReader();
+                    if (reader != null && reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            objRet = reader["Retorno"].ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return objRet;
+        }
     }
 }
